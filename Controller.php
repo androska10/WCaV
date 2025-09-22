2c7455d25321e6198cd799cddb8febed3791554b
androska <polesuke10@gmail.com>
2025-09-22 06:42:31 +0300

    Create Controller "MVC"

---
+++ Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . 'Logger';
+
+class Controller
+{
+    private $logger;
+
+    public function __construct()
+    {
+        $this->logger = new Logger();
+        $this->logger->info('Init Controller');
+    }
+
+    public function index()
+    {
+        $this->logger->info('Запрошена главная страница');
+
+        $filePath = __DIR__ . './View/index.html';
+
+        if (file_exists($filePath)) {
+            $this->logger->info('Отдаю HTML-страницу', ['file' => $filePath]);
+            readfile($filePath);
+            return true;
+        } else {
+            $this->logger->error('Файл не найден', ['file' => $filePath]);
+            http_response_code(404);
+            echo "Страница не найдена";
+            return false;
+        }
+    }
+}
