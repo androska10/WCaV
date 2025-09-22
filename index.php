f1a3c43724077e6a982d053bb2fd88d29c762332
androska <polesuke10@gmail.com>
2025-09-22 06:51:08 +0300

    Set index router
    Добавил вход и вызов контроллера
    
    Вывод первого представления

--- index.php
+++ index.php
@@ -8,3 +8,24 @@
  * 4. Вернуть ответ
  * 
  */
+
+require_once __DIR__ . '/src/Logger.php';
+require_once __DIR__ . '/src/Controller.php';
+
+$logger = new Logger();
+
+$logger->info("Приложение запущено",[
+    'request_uri' => $_SERVER['REQUEST_URI'] ?? 'CLI',
+    'request_method' => $_SERVER['REQUEST_METHOD'] ?? 'CLI'
+]);
+
+$uri = $_SERVER["REQUEST_URI"] ?? '/';
+
+if ($uri === '/' || $uri === '/index.php') {
+    $controller = new Controller ();
+    $controller->index();
+} else {
+    http_response_code(404);
+    $logger->warning("Страница не найдена", ['uri' => $uri]);
+    echo "<h1>404 - Страница не найдена</h1>";
+}
