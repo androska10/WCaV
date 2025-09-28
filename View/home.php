409147c98839cf790741d68d0e7d896afefe353e
androska <polesuke10@gmail.com>
2025-09-29 02:57:19 +0300

    Add Chart.js

--- View/home.php
+++ View/home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Binance Klines</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <h1>📊 Binance Klines (BTC/USDT)</h1>
